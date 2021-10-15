6bd8324f54d56f8af4daa83501c07428e94ce54c
emrahselek <furkanbasar17@gmail.com>
2021-10-15 13:28:15 -0400

    GoRestCoIn

--- src/test/java/stepDefinitions/ApiSteps.java
+++ src/test/java/stepDefinitions/ApiSteps.java
@@ -5,39 +5,48 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class ApiSteps {
-    @Given("user enters {string}")
-    public void userEnters(String arg0) {
+    @Given("user given api url {string}")
+    public void userGivenApiUrl(String url) {
+        System.out.println("user given api url " + url);
     }
 
-    @Given("set api enpoint")
-    public void setApiEnpoint() {
+    @Given("set api endpoint {string}")
+    public void setApiEndpoint(String endpoint) {
+        System.out.println("set api endpoint " + endpoint);
     }
 
-    @And("User creates new user with request body {string},{string},{string},")
-    public void userCreatesNewUserWithRequestBody(String name, String gender, String email) {
+    @And("User creates new user with request body {string},{string},{string},{string}")
+    public void userCreatesNewUserWithRequestBody(String name, String gender, String email, String status) {
+        System.out.println("User creates new user with request body " + name + " " + gender + " " + email + " " + status);
     }
 
     @Then("validate the status code {int}")
-    public void validateTheStatusCode(int arg0) {
+    public void validateTheStatusCode(int statusCode) {
+        System.out.println("validate the status code " + statusCode);
     }
 
     @And("validate the userId is not null")
     public void validateTheUserIdIsNotNull() {
+        System.out.println("validate the userId is not null");
     }
 
-    @And("validate the user name is  {string}")
-    public void validateTheUserNameIs(String arg0) {
+    @And("validate the user Name is {string}")
+    public void validateTheUserNameIs(String name) {
+        System.out.println("validate the user Name is " + name);
     }
 
     @And("validate the user Gender is {string}")
-    public void validateTheUserGenderIs(String arg0) {
+    public void validateTheUserGenderIs(String gender) {
+        System.out.println("validate the user Gender is " + gender);
     }
 
-    @And("validate the user Email is  {string}")
-    public void validateTheUserEmailIs(String arg0) {
+    @And("validate the user Email is {string}")
+    public void validateTheUserEmailIs(String email) {
+        System.out.println("validate the user Email is " + email);
     }
 
-    @And("validate the user status is {string}")
-    public void validateTheUserStatusIs(String arg0) {
+    @And("validate the user Status is {string}")
+    public void validateTheUserStatusIs(String status) {
+        System.out.println("validate the user Status is " + status);
     }
 }
